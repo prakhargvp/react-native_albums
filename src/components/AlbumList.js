a91267eb8c44547674f005f992731692cf831b6d
Prakhar Agrawal <prakhargvp@gmail.com>
2018-09-19 18:52:26 +0530

    Initial Commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class AlbumList extends Component {
+    componentWillMount() {
+        
+    }
+    render(){
+        return (
+            <View>
+                <Text>Album List!!!</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
